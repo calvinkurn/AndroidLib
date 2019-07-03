8d8b7e8b760d78fc454992590620ae764ff8d0ef
calvin.kurniawan@tokopedia.com <calvin.kurniawan@tokopedia.com>
2019-07-03 11:33:54 +0700

    initial toaster

---
+++ toaster/src/main/java/ToasterMessage.java
@@ -0,0 +1,8 @@
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void s(Context c,String s){
+        Toast.makeText(c,s,Toast.LENGTH_LONG).show();
+    }
+}
